548a184fb9a3ef07ecf8a9c2cb2513d020a94ffe
GH1BHARATH <blackblot03@gmail.com>
2022-02-15 22:04:28 +0530

    3 files modified, 2 added, 2 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { topBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ,ProductListComponent,],
+  declarations: [ AppComponent, HelloComponent ,ProductListComponent,topBarComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
