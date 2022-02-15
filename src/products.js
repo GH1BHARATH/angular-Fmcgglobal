b65ceec68f77c1d48a69590a01cd85ede8ddcf8e
GH1BHARATH <blackblot03@gmail.com>
2022-02-15 21:39:17 +0530

    1 file modified, 2 added

---
+++ src/products.js
@@ -0,0 +1,38 @@
+export const products
+[
+  {
+      "avalibility_QUALITY": 100,
+      "expiry_DATE": "2022-09-08",
+      "price": 50,
+      "product_id": "P0001",
+      "product_Name": "Britannia."
+  },
+  {
+      "avalibility_QUALITY": 100,
+      "expiry_DATE": "2022-10-08",
+      "price": 1,
+      "product_id": "P0002",
+      "product_Name": "Clinic Plus"
+  },
+  {
+      "avalibility_QUALITY": 100,
+      "expiry_DATE": "2022-10-08",
+      "price": 1,
+      "product_id": "P0003",
+      "product_Name": "Amul"
+  },
+  {
+      "avalibility_QUALITY": 100,
+      "expiry_DATE": "2022-10-08",
+      "price": 1,
+      "product_id": "P0004",
+      "product_Name": "Aavin"
+  },
+  {
+      "avalibility_QUALITY": 100,
+      "expiry_DATE": "2022-10-08",
+      "price": 1,
+      "product_id": "P0005",
+      "product_Name": "Colgate"
+  }
+]
