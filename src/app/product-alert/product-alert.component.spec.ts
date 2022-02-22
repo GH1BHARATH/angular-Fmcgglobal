b8f4a422b0d3485c26d4f20dbe0c77e2fa70ba87
GH1BHARATH <blackblot03@gmail.com>
2022-02-22 11:34:04 +0530

    6 files modified

--- src/app/product-alert/product-alert.component.spec.ts
+++ src/app/product-alert/product-alert.component.spec.ts
@@ -1,25 +1,25 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ProductAlertComponent } from './Product-alert.component';
+// import { ProductAlertComponent } from './Product-alert.component';
 
-describe('ProductAlertsComponent', () => {
-  let component: ProductAlertComponent;
-  let fixture: ComponentFixture<ProductAlertComponent>;
+// describe('ProductAlertsComponent', () => {
+//   let component: ProductAlertComponent;
+//   let fixture: ComponentFixture<ProductAlertComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ProductAlertComponent ]
-    })
-    .compileComponents();
-  });
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       declarations: [ ProductAlertComponent ]
+//     })
+//     .compileComponents();
+//   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ProductAlertComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(ProductAlertComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
